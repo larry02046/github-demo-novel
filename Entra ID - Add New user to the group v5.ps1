242597673d5601a64d7e0169e3a5f034139f17b5
larry02046 <44471851+larry02046@users.noreply.github.com>
2024-10-07 17:23:40 +1100

    Update Entra ID - Add New user to the group v5.ps1
    Update Entra ID - Add New user to the group v5.ps1

--- Entra ID - Add New user to the group v5.ps1
+++ Entra ID - Add New user to the group v5.ps1
@@ -1,5 +1,5 @@
 $sharepath  = '.\Transcripts'
-$datetime   = get-Date -f 'ddMMyyyy_HHmmss'
+$datetime   = get-Date -Format 'ddMMyyyy_HHmmss'
 $filename   = "Transcript_SR_${datetime}.txt"
 $Transcript = Join-Path -Path $sharepath -ChildPath $filename
 
@@ -17,48 +17,52 @@ catch {
 }
 
 # Define the timeframe for newly created users (e.g., last 1 day)
-$timeframe = (Get-Date).AddDays(-8)
+
+$timeframe = (Get-Date).AddDays(-15)
 Write-Host "Retrieving users created after $timeframe"
 
 # Get users created in the last 1 days
 
-
-
 $newUsers = Get-MgUser -Filter "accountEnabled eq true and OnPremisesSyncEnabled ne true and createdDateTime ge $($timeframe.ToString('yyyy-MM-ddTHH:mm:ssZ'))" -Property Displayname, UserPrincipalName, Id -ConsistencyLevel eventual -CountVariable CountVar | Select-Object DisplayName, UserPrincipalName, Id
-$newUsers
 
-if ($newUsers.Count -gt 0) {
+if ($newUsers.Count -gt 0) 
+{
     Write-Host "Found $($newUsers.Count) new users."
-    }
-    else {
-        Write-Host "No new users found."
-        exit
-    }
+    $newUsers | ForEach-Object {Write-Host $_.UserPrincipalName}
+    Write-Host "**********************"
+}
+else 
+{
+    Write-Host "No new users found."
+    exit
+}
 
-
-
-# Try to add the user to the group
 # Get current group members
 
 $groupMembers = Get-MgGroupMember -GroupId 9f252386-5efe-4144-8ac2-8dca893464b1
 
-# Check if the user is already a member of the group
-$isMember = $groupMembers | Where-Object { $_.Id -eq $user.Id }
+foreach ($user in $newUsers) {
+    $memberExists = $groupMembers | Where-Object { $_.Id -eq $user.Id }
 
-If ($isMember) {
-    Write-Host "Cannot add $($user.UserPrincipalName) because they are already the member of the group."
-    } else {
-        foreach ($user in $newUsers) {
-        Try {
+    If ($memberExists) 
+    {
+        Write-Host "Cannot add $($user.UserPrincipalName) because the user is already the member of the group."
+    }
+    else 
+    {
+        Try 
+        {
             Write-Host "Adding user - $($user.UserPrincipalName) - to 'NewStarter' Group"
             # Add each new user to the group
             New-MgGroupMember -GroupId 9f252386-5efe-4144-8ac2-8dca893464b1 -DirectoryObjectId $user.Id
-            Write-Host "$($user.UserPrincipalName) successfully added to 'NewStarter' group."
+            Write-Host "$($user.UserPrincipalName) is successfully added to 'NewStarter' group."
         }
-        catch {
+        catch 
+        {
             Write-error $error
         }
     }
 }
 
 Stop-Transcript
+
